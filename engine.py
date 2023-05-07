a5325fe3f9b0ce9cc4bf040a53f533d5427af5d2
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2023-05-07 21:59:40 +0200

    Initialize basic game components

--- engine.py
+++ engine.py
@@ -1,10 +1,25 @@
 import pygame
 
+from simulation_map.simulation_map import SimulationMap
+from utils.vec2 import Vec2
+from simulation_agent.simulation_agent import SimulationAgent
+from simulation_agent.civilization_type import CivilizationType
+
 class Engine():
     def __init__(self, config):
         self.config = config
 
         self.running = True
+
+        self.prepare()
+    
+    def prepare(self):
+        self.__map = SimulationMap(100, 100, 10, Vec2(10, 10), Vec2(90, 90))
+        
+        self.__agents = []
+        self.__agents += [SimulationAgent(self.__map, CivilizationType.BLUE, Vec2(10, 10))]
+        self.__agents += [SimulationAgent(self.__map, CivilizationType.RED, Vec2(90, 90))]
+
     
     def run(self):
         while self.running:
