e68ba669dfd82659950c8255bdf3a81eda68b782
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2023-05-07 22:49:06 +0200

    Finish run action

--- simulation_agent/actions/run_action.py
+++ simulation_agent/actions/run_action.py
@@ -1,5 +1,8 @@
 from simulation_agent.actions.action import Action
 from simulation_map.cell.cell_utils import CellUtils
+from utils.vec2 import Vec2
+
+import random
 
 class RunAction (Action):
     def areConditionsMet(self):
@@ -23,4 +26,14 @@ class RunAction (Action):
         return True
     
     def perform(self):
-        pass
+        moveVector = Vec2(
+            random.randint(-1, 1),
+            random.randint(-1, 1)
+        )
+
+        if moveVector.x == 0 and moveVector.y == 0:
+            moveVector.y = 1
+        
+        self._agent.move(moveVector)
+
+        self.finishAction()
