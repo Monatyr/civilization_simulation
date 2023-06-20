5000c6a33902193208461d4e4970f872e3650934
adam <adam.przywieczerski@gmail.com>
2023-06-21 00:43:40 +0200

    tweak in showPoints

--- showPoints.py
+++ showPoints.py
@@ -4,9 +4,15 @@ import utils.plots
 import utils.pickle
 
 picklePath = 'points_over_time.pkl'
+xlabel = 'Time'
+ylabel = 'Points'
+title = 'Points Over Time'
 
 if len(sys.argv) > 1:
     picklePath = sys.argv[1]
+    ylabel = 'Population'
+    title = 'Civilization population over time'
+
 
 loaded_pointsOverTime = utils.pickle.loadPickle(picklePath)
-utils.plots.renderPointsOverTime(loaded_pointsOverTime)
+utils.plots.renderOverTime(loaded_pointsOverTime, xlabel=xlabel, ylabel=ylabel, title=title)
